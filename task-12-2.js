6da771484ca671b53cb239615b3c4015bf9586f7
kosyanenkoks <kosyanenko.ks@gmail.com>
2019-02-26 02:18:40 +0200

    98% is done

--- task-12-2.js
+++ task-12-2.js
@@ -1,10 +1,12 @@
+var img = document.body.appendChild(document.createElement('img'));
+img.src = 'https://media.giphy.com/media/jcBp8Gx6AapxK/giphy.gif';
+
 function changeUrlImg() {
     var imageUrls = [
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST2kDAq7G6c6iNmn8RNQiRAXaZay_9xTvg3VjygsCzJBgYkqz_',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvVuBC90u-3cqOF6pwo5AJ1RE-mMjqmEA2T4-C8YClCQt72fF6vA',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXzKQ2A5KQMu4meQK3jNOtKKlUkgUh5lPulivsiCJ6GXJNjJu8Qw'
     ];
-    var img = document.body.appendChild(document.createElement('img'));
     var randomImgUrlIndex = Math.floor(Math.random() * imageUrls.length);
     var randomImgUrlVal = imageUrls[randomImgUrlIndex];
 
@@ -16,6 +18,11 @@ function changeUrlImg() {
             })
         });
 }
+
+// спасение
+window.onpopstate = function(event) {
+    changeUrlImg();
+};
 
 function changeHash(event) {
     var newHash = location.hash;
@@ -32,7 +39,6 @@ function changeHash(event) {
         'history',
         JSON.stringify(arrhistoryInfo)
     );
-    changeUrlImg();
     console.log('hash was changed');
 }
 
