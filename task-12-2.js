ff381ca8ee2da4f61ea23dc0c8e1f5e97f7cfec8
kosyanenkoks <kosyanenko.ks@gmail.com>
2019-03-04 22:23:57 +0200

    done

--- task-12-2.js
+++ task-12-2.js
@@ -1,15 +1,15 @@
 var img = document.body.appendChild(document.createElement('img'));
 img.src = 'https://media.giphy.com/media/jcBp8Gx6AapxK/giphy.gif';
+var imageUrls = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST2kDAq7G6c6iNmn8RNQiRAXaZay_9xTvg3VjygsCzJBgYkqz_',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvVuBC90u-3cqOF6pwo5AJ1RE-mMjqmEA2T4-C8YClCQt72fF6vA',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXzKQ2A5KQMu4meQK3jNOtKKlUkgUh5lPulivsiCJ6GXJNjJu8Qw'
+];
+var randomImgUrlIndex = Math.floor(Math.random() * imageUrls.length);
+var randomImgUrlVal = imageUrls[randomImgUrlIndex];
+var arrHistoryInfo = [];
 
 function changeUrlImg() {
-    var imageUrls = [
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST2kDAq7G6c6iNmn8RNQiRAXaZay_9xTvg3VjygsCzJBgYkqz_',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvVuBC90u-3cqOF6pwo5AJ1RE-mMjqmEA2T4-C8YClCQt72fF6vA',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXzKQ2A5KQMu4meQK3jNOtKKlUkgUh5lPulivsiCJ6GXJNjJu8Qw'
-    ];
-    var randomImgUrlIndex = Math.floor(Math.random() * imageUrls.length);
-    var randomImgUrlVal = imageUrls[randomImgUrlIndex];
-
     fetch(randomImgUrlVal)
         .then(response => {
             response.blob().then(response => {
@@ -27,12 +27,7 @@ function changeHash(event) {
         startTime: timeInSec
     };
 
-    // var prevHistoryInfo = JSON.parse(localStorage['history']);
-
-    var arrHistoryInfo = [];
     arrHistoryInfo.push(historyInfo);
-    // arrHistoryInfo.push(prevHistoryInfo);
-
     localStorage.setItem(
         'history',
         JSON.stringify(arrHistoryInfo)
